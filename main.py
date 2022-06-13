e3e4a502278a44ee698201bff06b055b225e8135
xtrcv <95971997+xtrcv@users.noreply.github.com>
2022-06-13 19:48:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,56 +5,24 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password_unscrambled = []
+# tuscias string kur bus talpinami slaptazodzio simboliai
+password_unscrambled = ""
 
-letters_to_add = []
+# trys apacioje esancios kilpos prides po raide, skaiciu, arba simboli is paciame virsuje esanciu sarasu
+for char in range(int(nr_letters)):
+   password_unscrambled += (str(random.choice(letters)))
 
-for char in range(nr_letters):
-  while nr_letters > len(letters_to_add):
-    letters_to_add.append(str(random.choice(letters)))
-    password_unscrambled.append(letters_to_add)
+for char in range(int(nr_symbols)):
+   password_unscrambled += (str(random.choice(symbols)))
 
-symbols_to_add = []
+for char in range(int(nr_numbers)):
+   password_unscrambled += (str(random.choice(numbers)))
 
-for sym in range(nr_symbols):
-  while nr_symbols > len(symbols_to_add):
-    symbols_to_add.append(str(random.choice(symbols)))
-    password_unscrambled.append(symbols_to_add)
+# password_unscrambled yra sumaisomas
+password_scrambled = ''.join(random.sample(password_unscrambled,len(password_unscrambled)))
 
-numbers_to_add = []
-
-for num in range(nr_numbers):
-  while nr_numbers > len(numbers_to_add):
-    numbers_to_add.append(str(random.choice(numbers)))
-    password_unscrambled.append(numbers_to_add)
-
-def listToString(stringAsList):
-  str = ""
-  for item in letters_to_add:
-    str += item
-  for item in symbols_to_add:
-    str += item
-  for item in numbers_to_add:
-    str += item
-  return str
-
-new_password_unscrambled = listToString(password_unscrambled)
-
-password_scrambled = []
-
-for char in new_password_unscrambled:
-  password_scrambled += random.choice(password_unscrambled)
-
-def listToStringFinal(password_scrambled):
-  str = ""
-  for item in password_scrambled:
-    str += item
-  return str
-
-final_password_scrambled = listToStringFinal(password_scrambled)
-
-print(f"Your password is: {final_password_scrambled}")
+print(f"Your password is: {password_scrambled}")
